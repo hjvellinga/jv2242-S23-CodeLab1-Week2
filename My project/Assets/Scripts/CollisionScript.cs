f481ce4a466f18ffe3572375a9dced154e3b60e2
hjvellinga <71023960+hjvellinga@users.noreply.github.com>
2023-02-09 16:22:51 -0500

    particle effect

--- My project/Assets/Scripts/CollisionScript.cs
+++ My project/Assets/Scripts/CollisionScript.cs
@@ -9,13 +9,20 @@ public class CollisionScript : MonoBehaviour
     //public int currentLevel = 0; -> should be in game manager
     // Start is called before the first frame update
     
-    //declare variable original position
-    public Vector2 originalPosPl1; 
+    //declare variable original position player 1
+    public Vector2 originalPosPl1;
+    //declare game object player 2?
+    public GameObject Player2;
+    //declare variable original position player 2
+    public Vector2 originalPosPl2; 
     void Start()
     {
-        //set originalPos player
+        //set Player2
+        Player2 = GameObject.Find("Player2"); //find the game object named "player2" in the scene ASK SOMEONE FOR HELP!!
+        //set originalPos player1 & player 2
 
-        originalPosPl1 = gameObject.transform.position; 
+        originalPosPl1 = gameObject.transform.position;
+        originalPosPl2 = Player2.transform.position;
     }
 
     // Update is called once per frame
@@ -27,10 +34,19 @@ public class CollisionScript : MonoBehaviour
     private void OnCollisionEnter2D(Collision2D col)
     {
         if (col.gameObject.name == "Player2")
+            
         {
+            //BUT ONLY ON COLLISION?!?
+            //GetComponent <ParticleSystem>().Play ();
+            //ParticleSystem.EmissionModule em = GetComponent<ParticleSystem>().emission;
+            //em.enabled = true;
+            //
+            
             SceneManager.LoadScene("Level2");
             gameObject.transform.position = originalPosPl1; //reset position Player 1.
-            //REPEAT FOR PLAYER 2!!!
+           
+            
+            
         }
     }
 }
